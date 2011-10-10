3cf0d774944b9f222052180ac102518ce712387d
David Mike Simon <david.mike.simon@gmail.com>
2011-10-10 10:43:15 -0700

    Can't reach rebar macros

--- src/rebar_efene_plugin.erl
+++ src/rebar_efene_plugin.erl
@@ -31,8 +31,6 @@
 
 -export([compile/2]).
 
--include("rebar.hrl").
-
 %% ===================================================================
 %% Public API
 %% ===================================================================
@@ -41,7 +39,7 @@ compile(Config, _AppFile) ->
     EfeneFirstFiles = rebar_config:get_list(Config, efene_first_files, []),
     IfeneFirstFiles = rebar_config:get_list(Config, ifene_first_files, []),
     rebar_base_compiler:run(Config, EfeneFirstFiles, "src", ".fn", "ebin", ".beam",
-                            fun compile_efene/3).
+                            fun compile_efene/3),
     rebar_base_compiler:run(Config, IfeneFirstFiles, "src", ".ifn", "ebin", ".beam",
                             fun compile_efene/3).
 
@@ -53,14 +51,15 @@ compile(Config, _AppFile) ->
 compile_efene(Source, _Target, Config) ->
     case code:which(fn) of
         non_existing ->
-            ?CONSOLE("~n===============================================~n" ++
-                     " You need to install Efene to compile Efene source.~n" ++
-                     "   Download it from here:~n" ++
-                     "   http://www.marianoguerra.com.ar/efene/#download~n" ++
-                     "   then build it, and finally make sure to~n" ++
-                     "   install it into your erlang library dir~n" ++
-                     "===============================================~n~n", []),
-            ?FAIL;
+            io:format(
+              "~n===============================================~n" ++
+              " You need to install Efene to compile Efene source.~n" ++
+              " Download it from here:~n" ++
+              "   http://www.marianoguerra.com.ar/efene/#download~n" ++
+              " and build it, and finally make sure to install it~n" ++
+              " into your erlang library dir.~n" ++
+              "===============================================~n~n", []),
+            throw({error, failed});
         _ ->
           fn:compile(Source, "ebin")
     end.
